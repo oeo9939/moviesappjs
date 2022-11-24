4a6bcc55def0eead1a765aab4ea38d6115c1e5d7
Teo <mgttmich@gmail.com>
2022-11-24 22:07:49 +0200

    Add html, css

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ async function getMovies() {
 
     console.log(respData);
 
+    // respData.results.forEach(movie => {
+    //     const img = document.createElement("img");
+    //     img.src = IMGPATH + movie.poster_path;
+
+    //     document.body.appendChild(img);
+    // });
+
     return respData;
 }
 
