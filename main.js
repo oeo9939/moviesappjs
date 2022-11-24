8c42c6af432488dc6c1ee50d0b724337b362e7e0
Teo <mgttmich@gmail.com>
2022-11-24 22:33:25 +0200

    Set html, css, add movieEl

--- main.js
+++ main.js
@@ -9,12 +9,27 @@ async function getMovies() {
 
     console.log(respData);
 
-    // respData.results.forEach(movie => {
-    //     const img = document.createElement("img");
-    //     img.src = IMGPATH + movie.poster_path;
+    respData.results.forEach((movie) => {
+        const { poster_path, title, vote_average} = movie;
 
-    //     document.body.appendChild(img);
-    // });
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+        <img src= "${IMGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span>${vote_average}</span>
+        </div>`;
+
+        // const img = document.createElement("img");
+        // img.src = IMGPATH + movie.poster_path;
+
+        // document.body.appendChild(img);
+
+        document.body.appendChild(movieEl);
+    });
 
     return respData;
 }
