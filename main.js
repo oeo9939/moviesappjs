9300b63b0a897764fb22e5314a7819614344d06f
Teo <mgttmich@gmail.com>
2022-11-25 10:27:30 +0200

    Add form.eventliste, func showMovies, change getMovies

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const APIKEY = '04c35731a5ee918f014970082a0088b1';
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280/';
-
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
 
 const main = document.querySelector("main");
-const form = document.querySelector("form");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
+// intially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
-    console.log(respData);
+    showMovies(respData.results);
+}
 
-    respData.results.forEach((movie) => {
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+    movies.forEach((movie) => {
         const { poster_path, title, vote_average} = movie;
 
 
@@ -34,8 +42,6 @@ async function getMovies() {
 
         main.appendChild(movieEl);
     });
-
-    return respData;
 }
 
 function getClassByRate(vote) {
@@ -48,5 +54,17 @@ function getClassByRate(vote) {
     }
 }
 
-getMovies();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
+
 
